9aa8fa6cd534d131dd6a370b6e95d3e60145a385
Marcus Oates <moates695@gmail.com>
2022-04-01 16:54:01 +1100

    q1 tests working

--- q1.py
+++ q1.py
@@ -10,25 +10,27 @@ N = 10
 argc = len(sys.argv)
 
 try:
-	db = psycopg2.connect("dbname=imdb")
-	if (argc != 3 or int(sys.argv[2]) <= 0):
-		raise ValueError
-	elif (argc == 3):
-		N = int(sys.argv[2])
-	cur = db.cursor()
-	qry = f"""select count(*), n.name from names as n
-			  inner join crew_roles as cr on n.id = cr.name_id
-			  where cr.role = 'director'
-			  group by n.name
-			  order by count(*) desc limit {N};"""
-	cur.execute(qry)
-	for tuple in cur.fetchall():
-		print(f"{tuple[0]} {tuple[1]}")
-	cur.close()
+    db = psycopg2.connect("dbname=imdb")
+    if (argc == 2):
+        N = int(sys.argv[1])
+        if (N <= 0):
+            raise ValueError
+    elif (argc > 2):
+        raise ValueError
+    cur = db.cursor()
+    qry = f"""select count(*), n.name from names as n
+inner join crew_roles as cr on n.id = cr.name_id
+where cr.role = 'director'
+group by n.name
+order by count(*) desc, n.name limit {N};"""
+    cur.execute(qry)
+    for tuple in cur.fetchall():
+        print(f"{tuple[0]} {tuple[1]}")
+    cur.close()
 except psycopg2.Error as err:
-	print("DB error: ", err)
+    print("DB error: ", err)
 except ValueError:
-	print(usage)
+    print(usage)
 finally:
-	if db:
-		db.close()
+    if db:
+        db.close()
