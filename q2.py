3c9df2ec1101d342b4dd009f13cd228a6ba2595d
Marcus Oates <moates695@gmail.com>
2022-04-01 15:42:05 +1100

    initial files

---
+++ q2.py
@@ -0,0 +1,26 @@
+# COMP3311 22T1 Ass2 ... print info about different releases for Movie
+
+import sys
+import psycopg2
+
+# define any local helper functions here
+
+# set up some globals
+
+usage = "Usage: q2.py 'PartialMovieTitle'"
+db = None
+
+# process command-line args
+
+argc = len(sys.argv)
+
+# manipulate database
+
+try:
+	db = psycopg2.connect("dbname=imdb")
+	# ... add your code here ...
+except psycopg2.Error as err:
+	print("DB error: ", err)
+finally:
+	if db:
+		db.close()
---
+++ q3.py
@@ -0,0 +1,26 @@
+# COMP3311 22T1 Ass2 ... print info about cast and crew for Movie
+
+import sys
+import psycopg2
+
+# define any local helper functions here
+
+# set up some globals
+
+usage = "Usage: q3.py 'MovieTitlePattern' [Year]"
+db = None
+
+# process command-line args
+
+argc = len(sys.argv)
+
+# manipulate database
+
+try:
+	db = psycopg2.connect("dbname=imdb")
+	# ... add your code here ...
+except psycopg2.Error as err:
+	print("DB error: ", err)
+finally:
+	if db:
+		db.close()
