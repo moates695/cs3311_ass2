c4441234661cb2b88977ef3947b96982166f4849
Marcus Oates <moates695@gmail.com>
2022-04-05 00:24:37 +1000

    q4 output looks correct but fails

--- q4.py
+++ q4.py
@@ -60,8 +60,7 @@ order by name, birth_year, id;"""
     elif len(tuples1) == 1:
         qry2 = f"""select round(avg(m.rating)::decimal, 1) from movies as m
 inner join principals as p on m.id = p.movie_id
-inner join names as n on p.name_id = n.id
-where n.id = {tuples1[0][0]};"""
+where p.name_id = {tuples1[0][0]};"""
         cur.execute(qry2)
         rating = cur.fetchone()[0]
         if rating == None:
@@ -71,18 +70,55 @@ where n.id = {tuples1[0][0]};"""
         qry3 = f"""select mg.genre from movie_genres as mg
 inner join movies as m on mg.movie_id = m.id
 inner join principals as p on m.id = p.movie_id
-inner join names as n on p.name_id = n.id
-where n.id = {tuples1[0][0]}
+where p.name_id = {tuples1[0][0]}
 group by genre
 order by count(*) desc, mg.genre;"""
         cur.execute(qry3)
         genres = clean(cur.fetchmany(3))
-        print(f"Filmography for {tuples1[0][1]} ({lifespan(tuples1[0])})")    
+        print(f"Filmography for {tuples1[0][1]} {lifespan(tuples1[0])}")    
         print("===============")
         print(f"Personal rating: {rating}")
         print(f"Top 3 Genres:")
         for genre in genres:
             print(f" {genre[0]}")
+        print("===============")
+        movies = {}
+        qry4 = f"""select m.id, m.title, m.start_year from movies as m
+inner join principals as p on m.id = p.movie_id
+where p.name_id = {tuples1[0][0]}
+order by m.start_year, m.title;"""
+        cur.execute(qry4)
+        tuples2 = clean(cur.fetchall())
+        for movie in tuples2:
+            movies[movie[0]] = {}
+            movies[movie[0]]['title'] = movie[1]
+            movies[movie[0]]['year'] = movie[2]
+            movies[movie[0]]['played'] = []
+            movies[movie[0]]['crew'] = []
+        qry5 = f"""select ar.movie_id, ar.played from acting_roles as ar
+where ar.name_id = {tuples1[0][0]}
+order by ar.played;"""
+        cur.execute(qry5)
+        acting_roles = clean(cur.fetchall())
+        for played in acting_roles:
+            if played[0] in movies.keys():
+                movies[played[0]]['played'].append(played[1])
+        qry6 = f"""select cr.movie_id, cr.role from crew_roles as cr
+where cr.name_id = {tuples1[0][0]}
+order by cr.role;"""
+        cur.execute(qry6)
+        crew_roles = clean(cur.fetchall())
+        for crew in crew_roles:
+            if crew[0] in movies.keys():
+                movies[crew[0]]['crew'].append(crew[1])
+        for movie in movies.values():
+            print(f"{movie['title']} ({movie['year']})")
+            if len(movie['played']) > 0:
+                for role in movie['played']:
+                    print(f" playing {role}")
+            if len(movie['crew']) > 0:
+                for role in movie['crew']:
+                    print(f" as {role[0].upper() + role[1:].replace('_', ' ')}")
     else:
         if year == None:
             print(f"Names matching '{sys.argv[1]}'")
